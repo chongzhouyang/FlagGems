9d1ef7d5b17b20310201e98f375203cf97cbf459
chongzhou.yang <chongzhou.yang@enflame-tech.com>
2026-07-13 09:55:02 +0000

    support qwen3

--- src/flag_gems/runtime/backend/_enflame/__init__.py
+++ src/flag_gems/runtime/backend/_enflame/__init__.py
@@ -37,8 +37,12 @@ if arch_version == 300:
     CUSTOMIZED_UNUSED_OPS = (
         "to_copy",
         "copy_",
+        "_to_copy",
     )
-elif arch_version == 400:
-    CUSTOMIZED_UNUSED_OPS = ("to_copy",)
+elif arch_version == 400 or arch_version == 410:
+    CUSTOMIZED_UNUSED_OPS = (
+        "to_copy",
+        "copy_",
+    )
 
 __all__ = ["*"]
